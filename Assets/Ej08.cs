d30a90d075d345880adf64d3e6ccc4bb87ec1cd8
facuzylberberg <101568863+facuzylberberg@users.noreply.github.com>
2022-04-05 08:33:37 -0300

    Ej08 finished

--- Assets/Ej08.cs
+++ Assets/Ej08.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class Ej08 : MonoBehaviour
 {
+    public int dia;
+
     // Start is called before the first frame update
     void Start()
     {
+        switch (dia) {
+
+            case 1: Debug.Log("Domingo");
+                break;
+            case 2: Debug.Ñog("Lunes");
+                break;
+            case 3: Debug.Log("Martes");
+                break;
+            case 4: Debug.Log("miercoles");
+                break;
+            case 5: Debug.Log("Jueves");
+                break;
+            case 6: Debug.Log("Viernes");
+                break;
+            case 7: Debug.Log("Sabado");
+                break;
+            default: Debug.Log("Su numero no crresponde");
+                break;
+
+        }
         
+   
     }
 
     // Update is called once per frame
